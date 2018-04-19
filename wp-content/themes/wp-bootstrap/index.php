8888746f120955a6b4981365319b73cfb24837c8
Vercillius <vercilliusjrmila@gmail.com>
2018-04-20 01:53:55 +0800

    added mega posy

--- wp-content/themes/wp-bootstrap/index.php
+++ wp-content/themes/wp-bootstrap/index.php
@@ -82,14 +82,20 @@
     </div>
     <div class="container-fluid">
         <div class="row" id="featured-post">  
+            <?php 
+                        
+                $the_query = new WP_Query( array('tag' => 'mega') );
+                
+                if( $the_query->have_posts()) : $the_query->the_post();?>
             <div class="col px-0">
                 <div class="jumbotron text-center">
                     
-                    <h1 class="title">Heading text</h1>
-                    <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
-                    <p><a class="btn btn-primary" role="button" href="#">Learn more</a></p>
+                    <h1 class="title"><?php the_title();?></h1>
+                    <p><?php the_content();?></p>
+                    <p><a class="btn btn-primary" role="button" href="<?php the_permalink();?>">Learn more</a></p>
                 </div>
             </div>
+        <?php endif;?>
         </div>
     </div>
     <div class="container">
