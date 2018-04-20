ffb03c808e44df75a49d70aa2c478ffc2654f352
Vercillius <vercilliusjrmila@gmail.com>
2018-04-20 23:01:01 +0800

    fixed the_content loop

--- wp-content/themes/wp-bootstrap/single.php
+++ wp-content/themes/wp-bootstrap/single.php
@@ -1,7 +1,8 @@
 <?php
 
 get_header();
-
+if(have_posts()):
+    while(have_posts()): the_post();
 ?>
 
 <div class="container-fluid">
@@ -22,14 +23,23 @@ get_header();
         </div>
     </div>
     <!-- End Nav -->
+    <!-- header -->
     <div class="row" id="post-image" style="background: linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(<?php the_post_thumbnail_url();?>); height:500px; background-position:center; background-size:cover;">
         <div class="col align-self-center text-center" id="post-header">
             <h1><?php the_title();?></h1>
         </div>
     </div>
-    
+    <!-- End Header -->
+    <!-- Content -->
+    <div class="row" id="single-the-content-container">
+        <div class="col">
+            <p><?php the_content();?></p>
+        </div>
+    </div>
 </div>
 
 <?php
+endwhile;
+endif;
 get_footer();
 ?>
