e525b15b9255b37213353b6929d2e21539e78785
Vercillius <vercilliusjrmila@gmail.com>
2018-04-23 21:40:30 +0800

    fixed post pic spacing

--- wp-content/plugins/northbuzz-widget/northbuzz-widget.php
+++ wp-content/plugins/northbuzz-widget/northbuzz-widget.php
@@ -18,7 +18,7 @@ function nb_widget_full_width_pic($atts){
      ), $atts);
 
      return "</div></div>
-                <div class='row '>
+                <div class='row nb-widget-pic-full-width'>
                     <div class='col'>
                         <img src='".$a['src']."' class='img-fluid'/>
                     </div>
@@ -33,7 +33,7 @@ function nb_widget_two_pic($atts){
         'src2' => ''
     ), $atts);
 
-    return "</div></div><div class='row '><div class=' col-xs-12 col-sm-6'><img src='".$a['src1']."' class='img-fluid'/></div><div class=' col-xs-12 col-sm-6' ><img src='".$a['src2']."' class='img-fluid'/></div></div><div class='row'><div class='col'>";
+    return "</div></div><div class='row nb-widget-pic-two'><div class=' col-xs-12 col-sm-6'><img src='".$a['src1']."' class='img-fluid'/></div><div class=' col-xs-12 col-sm-6' ><img src='".$a['src2']."' class='img-fluid'/></div></div><div class='row'><div class='col'>";
 }
 
  add_shortcode('nb_pic_full_width', 'nb_widget_full_width_pic');
